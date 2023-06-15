1cb2ef5c0f00d867a80044aca90af7003a6d7b9e
JamesFlores12 <jaimefloresfernandez60@gmail.com>
2023-06-15 10:51:43 -0400

    Prueba

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Home from './screens/Home.jsx'
-
+import {Route,BrowserRouter, Routes} from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/*<App />*/}
-    <Home/>
-  </React.StrictMode>,
+    
+    <BrowserRouter>
+        <Routes>
+          <Route path='/'element= {<Home/>}/>
+          <Route path='/peliculas'element= {<App/>}/>
+          <Route path='*' element={<p>Not found</p>}/>
+        </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
